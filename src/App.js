f053c5192924ca33336616ef5f1f64e2366f8d28
Sandra <sandranyambura62@gmail.com>
2022-10-07 19:57:55 +0300

    import navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,116 @@
 import './App.css';
+import Navbar from "./components/Navbar";
+
 
 function App() {
+  let mTodos = [
+    {
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  },
+    {
+      "userId": 1,
+      "id": 2,
+      "title": "quis ut nam facilis et officia qui",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 3,
+      "title": "fugiat veniam minus",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 4,
+      "title": "et porro tempora",
+      "completed": true
+    },
+    {
+      "userId": 1,
+      "id": 5,
+      "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 6,
+      "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 7,
+      "title": "illo expedita consequatur quia in",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 8,
+      "title": "quo adipisci enim quam ut ab",
+      "completed": true
+    },
+    {
+      "userId": 1,
+      "id": 9,
+      "title": "molestiae perspiciatis ipsa",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 10,
+      "title": "illo est ratione doloremque quia maiores aut",
+      "completed": true
+    },]
+
+  const [todos, setTodos] = useState(mTodos)
+
+  const toggleComplete = (id) => {
+    setTodos((prevState) => {
+      const filteredTodo = prevState.filter(todo => todo.id === id)
+      const editedTodo = {...filteredTodo[0], completed: !filteredTodo[0].completed}
+
+      const newTodos = prevState.filter(todo => todo.id !== id)
+      const result = [...newTodos, editedTodo]
+      return result.sort((a, b) => a.id - b.id);
+    })
+  }
+
+  const addTodo = (text) => {
+    const newTodo = {
+      id: todos.length + 1,
+      title: text,
+      completed: false
+    }
+
+    setTodos((prevState) => {
+      return [...prevState, newTodo]
+    })
+  }
+
+  const editTodo = (editedTodo) => {
+    setTodos((prevState) => {
+      const existingTodos = prevState.filter(todo => todo.id !== editedTodo.id)
+      return [...existingTodos, editedTodo].sort((a, b) => a.id - b.id);
+    })
+  }
+
+  const deleteTodo = (id) => {
+    setTodos(prevState => {
+      const remainingTodos = prevState.filter(todo => todo.id !== id)
+      return remainingTodos.sort((a, b) => a.id - b.id);
+    })
+  }
+
+  const [todoEdit, setTodoEdit] = useState(todos[0])
+
+  const setEdit = (todo) => {
+    console.log(todo)
+    setTodoEdit(todo)
+  }
+
   return (<div className="App">
     <Navbar />
     <BrowserRouter>
