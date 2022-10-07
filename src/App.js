054e377c0945fdb25d5fbbab97eae448ff8ee957
Sandra <sandranyambura62@gmail.com>
2022-10-07 19:56:41 +0300

    route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import './App.css';
 
 function App() {
   return (<div className="App">
-    
+    <Navbar />
+    <BrowserRouter>
+      <Routes>
+        <Route path={'/'} element={<Home />} />
+        <Route path={'/todo-list'}
+               element={<TodoList deleteTodo={deleteTodo} setTodoEdit={setEdit} todos={todos} toggleComplete={toggleComplete} />} />
+        <Route path={'/new-todo'} element={<NewTodo addTodo={addTodo} />} />
+        <Route path={'/edit-todo'} element={<TodoEdit editTodo={editTodo} todo={todoEdit} />} />
+      </Routes>
+    </BrowserRouter>
   </div>);
 }
 
