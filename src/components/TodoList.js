c781c18b4d161dcec72589664de8d6259b2cb46d
Sandra <sandranyambura62@gmail.com>
2022-10-07 19:41:43 +0300

    delete todo

---
+++ src/components/TodoList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Todo from "./Todo";
+
+function TodoList({todos, toggleComplete, setTodoEdit, deleteTodo}) {
+  return (
+    <ul className="list-group">
+      {todos.map(todo => (
+        <Todo deleteTodo={deleteTodo} todo={todo} toggleComplete={toggleComplete} setTodoEdit={setTodoEdit} />
+      ))}
+    </ul>
+  );
+}
+
+export default TodoList;
