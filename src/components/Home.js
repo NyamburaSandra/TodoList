1692c09719c835b73482afc0c5ae1d468b61fa23
Sandra <sandranyambura62@gmail.com>
2022-10-10 13:21:59 +0300

    done

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ function Home() {
   return (
     <div className="p-5 mb-4 bg-light rounded-3">
       <div className="container-fluid py-5">
-        <h1 className="display-5 fw-bold">TODO APP</h1>
+        <h1 className="display-5 fw-bold">Organise,Plan and Collaborate with ease</h1>
         <p className="col-md-8 fs-4">A simple tool to organise everything. It's a list of tasks you need to complete or things that you want to do. Helping you not forget anything important.</p>
         <button className="btn btn-primary btn-lg" type="button">
           <NavLink to={'/todo-list'}>See Todos</NavLink>
