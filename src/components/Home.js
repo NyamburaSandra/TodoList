b6ef6e51a6a2ab6d65355495d777752a0728d9aa
Sandra <sandranyambura62@gmail.com>
2022-10-10 13:18:07 +0300

    make changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,10 +5,8 @@ function Home() {
   return (
     <div className="p-5 mb-4 bg-light rounded-3">
       <div className="container-fluid py-5">
-        <h1 className="display-5 fw-bold">Welcome To My TODO LIST</h1>
-        <p className="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in
-          previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your
-          liking.</p>
+        <h1 className="display-5 fw-bold">TODO APP</h1>
+        <p className="col-md-8 fs-4">A simple tool to organise everything. It's a list of tasks you need to complete or things that you want to do. Helping you not forget anything important.</p>
         <button className="btn btn-primary btn-lg" type="button">
           <NavLink to={'/todo-list'}>See Todos</NavLink>
         </button>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ function Navbar(props) {
   return (
     <nav className="navbar navbar-expand-lg bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">Todo-APP</a>
+        <a className="navbar-brand" href="/">Todo App</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
